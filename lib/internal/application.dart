d16f5d12f95ccbd87eea6447651eb463b4acf81a
Evgenii Bondarev <evgeniy.bondarev@irlix.ru>
2021-11-14 22:22:43 +0300

    Init.

---
+++ lib/internal/application.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_architecture/ui/home.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Architecture',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const Home(),
+    );
+  }
+}
